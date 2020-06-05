21634d1d15a5240e52b140c99da40338e6945f5b
mahesh_sudhakar <mahesh.sudhakar@autodrive.utoronto.ca>
2020-06-05 01:13:33 -0400

    count added -  message filter - June 05 01:15 AM

--- asynchronous.py
+++ asynchronous.py
@@ -1,20 +1,24 @@
+# import necessary packages
+
 import message_filters
 import rospy
-import os
-from std_msgs.msg import Int32, Float32
-
 from sensor_msgs.msg import Imu, PointCloud2
 from nav_msgs.msg import Odometry
 
-pwd = '/home/autoronto/calibre_ws/src/msgs_to_file/src'
 
 def callback(imu, gps, pcl):
-  # The callback processing the pairs of numbers that arrived at approximately the same time
+  """
+	[INFO] callback() function
 
-  #rospy.loginfo(imu.header.seq)
-  #rospy.loginfo(gps.header.seq)
-  #rospy.loginfo(pcl.header.seq)
-  
+	@params  : 
+	   imu, gps, pcl : Subscriber object
+  """
+
+  global count 
+
+  count += 1
+  print('Count : '+str(count))
+
   with open('approx_synchronous.csv', 'a+') as file:
   	file.write(
 			str(imu.header.stamp.secs) + ', ' + str(imu.header.stamp.nsecs) + ', ' + str(imu.orientation.x) +  ', ' + str(imu.orientation.y) + ', ' + str(imu.orientation.z) 
@@ -29,7 +33,11 @@ imu_sub = message_filters.Subscriber('/imu/data', Imu)
 gps_sub = message_filters.Subscriber('/navsat/odom', Odometry)
 pcl_sub = message_filters.Subscriber('/velodyne_points', PointCloud2)
 
+
 rospy.init_node('spot_recorder', log_level=rospy.INFO) 
 ts = message_filters.ApproximateTimeSynchronizer([imu_sub, gps_sub, pcl_sub], 10, 0.1, allow_headerless=True)
+
+count = 0
 ts.registerCallback(callback)
+
 rospy.spin()
